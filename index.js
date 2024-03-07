3cd7d42a9fcaf8ce64269238c7db300b096c8b04
Maneesha09-87 <ramapurammaneesha@gmail.com>
2024-03-07 22:35:46 +0530

    Arrays in JS

---
+++ index.js
@@ -0,0 +1,37 @@
+//arrays
+//list-like information
+//an array--a special kind of object that stores list-like information
+let array=[]
+//creating an array
+
+let groceriesBuy=['cabbage','tamota']
+
+//array values
+const string=[1,2,3,4]
+console.log(string.length)
+
+//getting the value of an item
+let fir=string[0]
+console.log(fir)
+let a1=string[string.length-1]
+console.log(a1)
+//setting the value of an item
+string[0]=10
+console.log(string)
+ //finding the position of the item in an array
+ let index=string.indexOf(2)
+ console.log(index)
+ //removing items from the front
+ const string=[1,2,3,4]
+ let aq=string.slice(2)  //3,4
+ console.log(aq)
+ //
+ let aq1=string.slice()
+ //write array using persons
+ 
+ const string1=['Mahatma gandhi','nehru','alluri','rani lakshmi']
+ console.log(string1.length)
+ let index1=string.indexOf(1)
+ console.log(index1)
+ let a=string1.slice(2)
+ console.log(a)
